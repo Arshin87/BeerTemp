b49a42e8d6483efee66dc2fb94c471d2b2771cb3
Arshin87 <torbjorn.pb@gmail.com>
2018-07-29 17:43:24 +0200

    working on button 1

--- test.php
+++ test.php
@@ -117,7 +117,7 @@
         <script>
         	function toggle(){
         		var x = document.getElementById("listTemps");
-        		if (x.display === "none"){
+        		if (x.display != "none"){
         			x.style.display = "block";
         		} else {
         			x.style.display = "none";
